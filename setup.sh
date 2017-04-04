62971ace7cb735d3f5d88c885ddbb104d7abf21d
Billy Hardy <hardywb@leidos.com>
2017-04-04 14:16:09 -0400

    add setup file

---
+++ setup.sh
@@ -0,0 +1,5 @@
+ln -s .bash_profile dotfiles/bash_profile
+ln -s .bash_aliases dotfiles/bash_aliases
+ln -s .zshrc dotfiles/zshrc
+ln -s .gitconfig dotfiles/gitconfig
+ln -s .git_template dotfiles/git_template
