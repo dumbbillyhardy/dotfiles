ef8e9292650381613d063a76eb40d525afc7ff32
Billy Hardy <billy92@gmail.com>
2019-05-25 15:52:06 -0400

    add install.sh to apt install software

---
+++ installs.sh
@@ -0,0 +1,17 @@
+# tmux
+sudo apt install tmux
+
+# bazel
+sudo apt-get install openjdk-8-jdk
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+
+sudo apt-get update && sudo apt-get install bazel
+
+# rust
+curl https://sh.rustup.rs -sSf | sh
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+## install latest version
+nvm install node
