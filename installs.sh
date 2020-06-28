f51e58e18d49f918fbf3235412b9920b96dc9703
Billy Hardy <billy92@gmail.com>
2020-06-27 21:40:59 -0400

    add you complete me install

--- installs.sh
+++ installs.sh
@@ -21,3 +21,7 @@ nvm install node
 ##plugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+##YouCompleteMe (and dependencies)
+sudo apt install build-essential cmake vim python3-dev
+(cd .vim/plugged/YouCompleteMe; python3 install.py --all)
